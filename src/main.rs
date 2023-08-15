c6566cbdb275e70797ecc5b73cac527a6ba09cde
RistoFlink <kenssi@gmail.com>
2023-08-15 22:47:12 +0300

    Remove closure from the hello router

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,29 @@
 use std::net::SocketAddr;
 use std::sync::mpsc::RecvTimeoutError;
 
-use axum::{Router, routing::get};
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
+use axum::{routing::get, Router};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-    get(|| async { Html("Hello, <strong> world!</strong>")})
-    ); 
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("-> LISTENING on {addr}\n");
-    axum::Server::bind(&addr).serve(routes_hello.into_make_service()).await.unwrap();
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello <strong>world</strong>")
 }
