e2590f271ffc077f736b195d1aa7379f95eb9001
RistoFlink <kenssi@gmail.com>
2023-09-20 15:12:22 +0300

    Add first extractor

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,13 +1,21 @@
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
-use tower_cookies::Cookies;
+use axum::{async_trait, RequestPartsExt};
 use lazy_regex::regex_captures;
+use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
-pub async fn mw_require_auth<B>(cookies: Cookies, req: Request<B>, next: Next<B>) -> Result<Response>{
+pub async fn mw_require_auth<B>(
+    cookies: Cookies,
+    req: Request<B>,
+    next: Next<B>,
+) -> Result<Response> {
     println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
 
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
@@ -17,13 +25,29 @@ pub async fn mw_require_auth<B>(cookies: Cookies, req: Request<B>, next: Next<B>
     Ok(next.run(req).await)
 }
 
+//Ctx extractor
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "EXTRACTOR");
+        //use the cookie extractor
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        Ok((Ctx::new(user_id)))
+    }
+}
+
 //parse a token of format `user-[user-id].[expiration].[signature]`
 //returns (user_id, expiration, signature)
-fn parse_token(token: String) -> Result<(u64, String, String)>{
-    let (_whole, user_id, exp, sign) = regex_captures!(
-        r#"^user-(\d+)\.(.+)\.(.+)"#,
-        &token
-        ) 
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+    let (_whole, user_id, exp, sign) = regex_captures!(r#"^user-(\d+)\.(.+)\.(.+)"#, &token)
         .ok_or(Error::AuthFailTokenWrongFormat)?;
 
     let user_id: u64 = user_id
