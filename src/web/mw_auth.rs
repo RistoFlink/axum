b4cc7e09d2f83ea83e8c27dd7409bde2dff5f32f
RistoFlink <kenssi@gmail.com>
2023-09-20 15:25:21 +0300

    Add Ctx in REST APIs

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -12,16 +12,14 @@ use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
 pub async fn mw_require_auth<B>(
-    cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>,
     next: Next<B>,
 ) -> Result<Response> {
     println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
 
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
+    ctx?;
+
     Ok(next.run(req).await)
 }
 
